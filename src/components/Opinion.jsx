99942f774be847d29fe739c6bf9a440d8e1e734d
richardborgesdev <richardborgesdev@gmail.com>
2025-07-21 14:59:37 -0300

    chore: apply useAction for down and up votes

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
@@ -12,6 +12,11 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
     await useOpinionsContext.downvoteOpinion(id);
   }
 
+  const [upvoteFormState, upvoteFormAction, upvotePending] =
+    useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] =
+    useActionState(downvoteAction);
+
   return (
     <article>
       <header>
@@ -20,7 +25,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upvoteAction}>
+        <button
+          formAction={upvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -40,7 +48,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button formAction={downvoteAction}>
+        <button
+          formAction={downvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
